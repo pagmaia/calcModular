b26b8ed7c4b0eeed336486cdedd6ed4540b0eadc
pagmaia <pedroarthurgmaia@gmail.com>
2026-03-13 17:03:07 -0300

    Interatividade e operações implementadas

--- circulomod.js
+++ circulomod.js
@@ -1,9 +1,52 @@
-let n = 12;
-let a = 20;
 let anguloGirado = 0;
-let bool = false;
-const draw = document.getElementById("relogio");
-const obj = draw.getContext("2d"); 
+
+function getOpcao(){
+    const op = document.getElementById("operacoes");
+    const v = op.value;
+    op.value = "";
+    return v;
+}
+
+function apresentar(valores){
+    let n = 0;
+    let a = 0;
+    let b = 0;
+
+    resetar();
+    n = parseInt(valores["modn"]);
+    if(n < 25){
+        desenharCirculoMod(n);
+
+        a = parseInt(valores["numA"]);
+        criarPonteiro();
+        numA.value = "";
+        const opt = getOpcao();
+        b = parseInt(valores["numB"]);
+
+        if(opt == "add"){
+            moverNVezes(a + b, 360 / n, 400);
+        }
+        else if(opt == "multi"){
+            const angulo = a * (360 / n);
+            moverNVezes(b, angulo, 800)
+        }
+        else if(opt == "expo"){
+             angulo = 360 / n;
+            moverNVezesExponencial(a, b, n, angulo, 1000);
+        }
+        numB.value = "";
+    }
+    
+}
+
+function getInputs(){
+    const numeros = {};
+    const valores = document.querySelectorAll(".interface .input");
+    for(const val of valores){
+        numeros[val.id] = val.value;
+    }
+    return numeros;
+}
 
 function moverGrau(angulo){
     const ponteiro = document.getElementById("ponteiro");
@@ -12,14 +55,28 @@ function moverGrau(angulo){
 
 function mudarTiming(ms){
     const ponteiro = document.getElementById("ponteiro");
-    ponteiro.style.setProperty("--transition-duration", toString(ms));
+    ponteiro.style.transition = `transform ${ms}ms ease-out`;
 }
 
 function moverNVezes(n, angulo, timing){
-    mudarTiming(timing);
+    mudarTiming(timing - 100);
     for(let i = 1; i <= n; i++){
         setTimeout(() =>{
             anguloGirado += angulo;
+            moverGrau(anguloGirado); 
+        }, i * timing);
+    }
+}
+
+function moverNVezesExponencial(a, b, n, base, timing){
+    mudarTiming(timing - 100);
+    for(let i = 1; i <= b; i++){
+        const valor = Math.pow(a, i);
+        const posicao = valor % n;
+        const angulo = posicao * base;
+
+        setTimeout(() =>{
+            anguloGirado += (angulo - anguloGirado + 360) % 360;
             moverGrau(anguloGirado); 
         }, i * timing);
     }
@@ -30,16 +87,19 @@ function criarPonteiro(){
     ponteiro.id = "ponteiro";
     ponteiro.classList.add("ponteiro");
     document.querySelector(".circuloMod").appendChild(ponteiro);
+}   
+
+function removePonteiro(){
+    document.getElementById("ponteiro").remove();
 }
 
-function desenharCirculoMod(){
-    if(bool){
-        return;
-    }
+function desenharCirculoMod(n){
+    const draw = document.getElementById("relogio");
+    const obj = draw.getContext("2d"); 
 
     const centrox = draw.width / 2;
     const centroy = draw.height / 2;
-    const raio = 200;
+    const raio = (draw.width / 2) - 150;
     obj.strokeStyle = "black";
     obj.font = "20px Helvetica"
     obj.lineWidth = 5;
@@ -52,7 +112,6 @@ function desenharCirculoMod(){
     obj.arc(centrox, centroy, 2, 0, 2 * Math.PI);
     obj.stroke();
 
-    obj.strokeRect(0, 0, draw.width, draw.height)
     obj.textAlign = "center";
     obj.textBaseline = "middle";
     
@@ -74,11 +133,34 @@ function desenharCirculoMod(){
         obj.lineTo(xStroke, yStroke);
         obj.stroke();
     }
-
-    let angulo = 360 / n;
-    criarPonteiro();
-    moverNVezes(n * (parseInt(a / n)) + a % n, angulo, 500);
-    
-    bool = true;
-
 }
+
+function resetar(){
+    const draw = document.getElementById("relogio");
+    const obj = draw.getContext("2d");
+    obj.clearRect(0, 0, draw.width, draw.height);
+
+    const ponteiro = document.getElementById("ponteiro");
+    if(ponteiro){
+        ponteiro.remove();
+    }
+    anguloGirado = 0;
+}
+
+function resetarPonteiro(){
+    const ponteiro = document.getElementById("ponteiro");
+    if(ponteiro){
+        ponteiro.remove();
+    }
+    anguloGirado = 0;
+}
+
+function main(){
+    document.addEventListener("keydown", function(event){
+        if(event.key === "Enter"){
+            const valores = getInputs();
+            apresentar(valores);
+        }
+    })
+}
+main();
